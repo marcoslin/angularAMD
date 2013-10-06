ecdf13e7f831dd7937a0d1afde951a7a2fde9148
Marcos Lin <linm.linmmac@ebacimail.com>
2013-10-06 22:06:48 +0200

    Finalize the minimize script adding test after minimization

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,14 @@ module.exports = function (grunt) {
     'use strict';
     require('load-grunt-tasks')(grunt);
     
+    // Config variables
+    var configVars = {
+        "build": "build",
+        "dist": "dist"
+    }
+    
     grunt.initConfig({
+        cvars: configVars,
         bower: {
             install: {
                 options: { install: true, copy: false }
@@ -20,14 +27,76 @@ module.exports = function (grunt) {
             }
         },
         karma: {
-            unit: {
-                configFile: 'test/conf/karma.unit.js'
+            "unit": {
+                configFile: '<%= cvars.build %>/test/conf/karma.unit.js'
+            },
+            "build": {
+                configFile: '<%= cvars.build %>/test/conf/karma.unit.js',
+                singleRun: true
+            },
+            "build-min": {
+                configFile: '<%= cvars.build %>/test/conf/karma.unit.min.js',
+                singleRun: true
             }
         },
+        template: {
+            "main-js": {
+                src: 'test/unit/lib/main.mustache',
+                dest: '<%= cvars.build %>/test/unit/lib/main.js',
+                variables: {
+                    "angularAMD-js-file": "src/angularAMD"
+                }
+            },
+            "main-min-js": {
+                src: 'test/unit/lib/main.mustache',
+                dest: '<%= cvars.build %>/test/unit/lib/main.min.js',
+                variables: {
+                    "angularAMD-js-file": "<%= cvars.build %>/angularAMD.min"
+                }
+            },
+            "karma-js": {
+                src: 'test/conf/karma.unit.mustache',
+                dest: '<%= cvars.build %>/test/conf/karma.unit.js',
+                variables: {
+                    "main-js-file": "<%= cvars.build %>/test/unit/lib/main.js"
+                }
+            },
+            "karma-min-js": {
+                src: 'test/conf/karma.unit.mustache',
+                dest: '<%= cvars.build %>/test/conf/karma.unit.min.js',
+                variables: {
+                    "main-js-file": "<%= cvars.build %>/test/unit/lib/main.min.js"
+                }
+            }
+        },
+        uglify: {
+            build: {
+                options: {
+                    'report': true
+                },
+                files: {
+                    '<%= cvars.build %>/angularAMD.min.js': ['src/angularAMD.js']
+                }
+            }
+        }
     });
     
     grunt.registerTask('setup', ['bower:install', 'copy:bower-www']);
-    grunt.registerTask('test', ['karma:unit']);
+    grunt.registerTask('test', [
+        'template:main-js','template:karma-js',
+        'karma:unit'
+    ]);
+    
+    /**
+     * build the file, by testing the src/angularAMD.js first, minimize it, then test the minimized version
+     */
+    grunt.registerTask('build', [
+        'template:main-js','template:karma-js',
+        'template:main-min-js','template:karma-min-js',
+        'karma:build',
+        'uglify:build',
+        'karma:build-min'
+    ]);
     
 };
 
