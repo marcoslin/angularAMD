76da4d95346175848999d19dcbaf3a242f11c88c
Marcos Lin <linm.linmmac@ebacimail.com>
2013-10-09 20:12:32 +0200

    Removed the unused domReady and added local ref to angular-route

--- www/js/main.js
+++ www/js/main.js
@@ -5,9 +5,8 @@ require.config({
 	// alias libraries paths
     paths: {
         'angular': '../lib/angular/angular',
-        'angular-route': '//ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular-route.min',
+        'angular-route': '../lib/angular/angular-route',
         'async': '../lib/requirejs/async',
-        'domReady': '../lib/requirejs/domReady',
         'angularAMD': '../lib/requirejs/angularAMD',
         'ui-bootstrap': '../lib/angular-ui-bootstrap/ui-bootstrap-tpls',
 
