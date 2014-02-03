d7cb00fcd7072f3371b11700a9b200b3f1e78c64
Marcos Lin <linm.linmair@ebacimail.com>
2014-02-03 19:55:32 +0100

    WIP: initial working version of protractor

---
+++ test/conf/e2e.spec.js
@@ -0,0 +1,52 @@
+var url = require("url");
+
+describe('angularAMD', function() {
+    var ptor = protractor.getInstance();
+    
+    /**
+     * Function used to in place of `ptor.get` as the native version will not wait for manual bootstrapping.
+     * The solution is to wait for wait for URL to be loaded
+     */
+    function ptor_get(rel_path) {
+        ptor.driver.get(url.resolve(ptor.baseUrl, rel_path));
+        ptor.wait(function () {
+            waits(500);
+            return ptor.driver.getCurrentUrl().then(function(in_url) {
+                var re = new RegExp(rel_path, "i");
+                return re.test(in_url);
+            });
+        }, 5000, "Taking too long to load " + rel_path);
+    }
+    
+    describe("home", function () {
+        beforeEach(function () {
+            ptor_get('#/home');
+        });
+        
+        it('should redirect to home', function() {
+            expect(ptor.getCurrentUrl()).toContain('#/home');
+        });
+        
+        it("View on GitHub button should exists", function () {
+            expect($("#view-on-github").getAttribute('class')).toBe("btn btn-large");
+        });
+        
+        it("View on GitHub button should exists", function () {
+            expect($("#view-on-github").getAttribute('class')).toBe("btn btn-large");
+            expect($("#view-on-github i").getAttribute('class')).toBe("icon-github-sign");
+        });
+    });
+
+    describe("module", function () {
+        beforeEach(function () {
+            ptor_get('#/modules');
+        });
+        
+        it("ng-write", function () {
+            "output-ng-write"
+            expect($("#output-ng-write").getText()).toBe("Output from Directive");
+        })
+    
+    });
+    
+});
---
+++ test/conf/protractor.e2e.js
@@ -0,0 +1,22 @@
+// An example configuration file.
+exports.config = {
+    // The address of a running selenium server.
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    
+    baseUrl: 'http://localhost:8444',
+    
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['e2e.spec.js'],
+    
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    }
+};
