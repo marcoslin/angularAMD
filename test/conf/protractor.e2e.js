3e600213e6a901e7d6251b567cf70748265b9e06
Marcos Lin <linm.linmair@ebacimail.com>
2014-02-04 09:52:59 +0100

    Cleaned up the files naming for E2E tests and removed legacy files.

--- test/conf/protractor.e2e.js
+++ test/conf/protractor.e2e.js
@@ -12,7 +12,7 @@ exports.config = {
     
     // Spec patterns are relative to the current working directly when
     // protractor is called.
-    specs: ['e2e.spec.js'],
+    specs: ['../e2e/*.spec.js'],
     
     // Options to be passed to Jasmine-node.
     jasmineNodeOpts: {
