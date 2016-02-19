27065ea6d8b91b3b0de7f0bec7f9ce6cb11c6b11
Ben Elliott <ben.elliott@gsacapital.com>
2016-02-19 08:57:00 +0000

    Only alias .component if found on $compileProvider

--- src/angularAMD.js
+++ src/angularAMD.js
@@ -394,7 +394,7 @@ define(function () {
                     $provide: provide
                 };
 
-                // Substitue provider methods from app call the cached provider
+                // Substitute provider methods from app call the cached provider
                 angular.extend(onDemandLoader, {
                     provider : function(name, constructor) {
                         provide.provider(name, constructor);
@@ -409,8 +409,7 @@ define(function () {
                         return this;
                     },
                     component : function(name, constructor) {
-                        compileProvider.component(name, constructor);
-                        return this;
+                        // do nothing by default
                     },
                     filter : function(name, constructor) {
                         filterProvider.register(name, constructor);
@@ -435,6 +434,17 @@ define(function () {
                     },
                     animation: angular.bind(animateProvider, animateProvider.register)
                 });
+
+                // Only add component method if present on compileProvider (i.e. Angular >= v.1.5):
+                if (compileProvider.component) {
+                    angular.extend(onDemandLoader, {
+                        component : function(name, constructor) {
+                            compileProvider.component(name, constructor);
+                            return this;
+                        }
+                    });
+                }
+                
                 angular.extend(alt_app, onDemandLoader);
 
             }]
